c3e03e9ddee3f04c1d2c9861b89b8f890e1dc2fd
Mykola Balan <balann705@ukr.net>
2022-03-18 16:55:38 +0200

    First commit (everything);

---
+++ src/com/train/MainClass.java
@@ -0,0 +1,43 @@
+package com.train;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("мама успокойся , это всего чеснок !!!! \r");
+        char h ='\u4534';
+        int 
+        int a =10 ;
+        int b =071;
+        int c = 047;
+        int t =0x10101010;
+        int r
+
+        System.out.println(h);
+
+        int height = 80 ;
+        First element = new First();
+        System.out.println(element.arr);
+        int [] arr = new int [5];
+        int arrr[] = new int [6];
+        int [] t = {5,6,8,9,0};
+        int f [][] =new int [3][3];
+        f[2]= new int [7];
+
+
+    }
+}
+class First {
+    int arr;
+    private int weight ;
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+    public int getWeight() {
+        return weight;
+    }
+    public First (){
+        arr =9;
+    }
+    {
+        arr =8;
+    }
+}
